7144536f2c8cb3ffdfde392f7f277451b9801cc6
Romain <37015399+FoxinouFR@users.noreply.github.com>
2024-04-08 23:05:24 +0200

    Comment BACKEND initialization

--- sentinel-installer.sh
+++ sentinel-installer.sh
@@ -42,7 +42,7 @@ function load_config_files()
 	V2RAY_PORT=$(cat ${USER_HOME}/.sentinelnode/v2ray.toml | grep "^listen_port\s*=" | awk -F" = " '{print $2}' | tr -d '"')
 	CHAIN_ID=$(cat ${USER_HOME}/.sentinelnode/config.toml | grep "^id\s*=" | awk -F"=" '{print $2}' | tr -d '"')
 	RPC_ADDRESSES=$(cat ${USER_HOME}/.sentinelnode/config.toml | grep "^rpc_addresses\s*=" | awk -F" = " '{print $2}' | tr -d '"')
-	BACKEND=$(cat ${USER_HOME}/.sentinelnode/config.toml | grep "^backend\s*=" | awk -F" = " '{print $2}' | tr -d '"')
+	# BACKEND=$(cat ${USER_HOME}/.sentinelnode/config.toml | grep "^backend\s*=" | awk -F" = " '{print $2}' | tr -d '"')
 	HOURLY_PRICES=$(cat ${USER_HOME}/.sentinelnode/config.toml | grep "^hourly_prices\s*=" | awk -F" = " '{print $2}' | tr -d '"')
 
 	# if hourly_prices egale to DATACENTER_HOURLY_PRICES
